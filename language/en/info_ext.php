a645f5c72cccd12964d5295b2e51e0dde75136cd
Andy Vandenberghe <andreasvdberghe@avathar.be>
2026-06-13 18:56:53 +0200

    fix lang in ext.php

---
+++ language/en/info_ext.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Star Wars: The Old Republic game plugin for bbGuild.
+ *
+ * @copyright (c) 2026 avathar.be
+ * @license GNU General Public License, version 2 (GPL-2.0)
+ */
+
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+$lang = array_merge($lang, array(
+	// is_enableable() error messages
+	'BBGUILDSWTOR_PHP_VERSION_FAIL'		=> 'This extension requires PHP %1$s or higher. You are running PHP %2$s.',
+	'BBGUILDSWTOR_PHPBB_VERSION_FAIL'	=> 'This extension requires phpBB %1$s or higher. You are running phpBB %2$s.',
+	'BBGUILDSWTOR_REQUIRES_BBGUILD'		=> 'This extension requires the bbGuild core extension (avathar/bbguild) to be enabled first.',
+));
